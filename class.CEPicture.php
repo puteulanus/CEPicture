d3dfdda9963f00c8bdca62a778ae761d62effefb
Puteulanus <mj19941230@gmail.com>
2014-12-04 01:52:07 +0800

    Create class.CEPicture.php

---
+++ class.CEPicture.php
@@ -0,0 +1,102 @@
+<?php
+class CEpiture{
+    public $api_url = '';
+    const CE_URL = 'https://cefamilie.com';
+    public $version = '0.1';
+    private $cookie = '';
+    private $page_content;
+    function __construct(){
+    }
+    
+    function CeLoadPage($page_number){
+        $this->page_content = file_get_contents(self::CE_URL."/page/{$page_number}");
+    }
+    
+    function GetMangaList(){
+        if (!$this->page_content){return false;}
+        preg_match('/<div class="mainleft">(?:.|\n)+?<\/ul>/',$this->page_content,$result);
+        preg_match_all('/<a href="'.str_replace('/','\/',self::CE_URL).'\/(?<id>\d+)" class="zoom" rel="bookmark" title="(?<title>.+?)">/',$result[0],$result);
+        $result['result'] = array();
+        foreach($result['title'] as $id => $title){
+            $result['result'] += array($result['id'][$id] => $title);
+        }
+        return $result['result'];
+    }
+    
+    function GetMangaCover($size,$type){
+        if (!$this->page_content){return false;}
+        switch ($type){
+            case 'w':
+                $size = "&amp;w={$size}&amp;zc=1+;a=t";
+                break;
+            case 'h':
+                $size = "&amp;h={$size}&amp;zc=1+;a=t";
+                break;
+            default:
+                return false;
+        }
+        preg_match('/<div class="mainleft">(?:.|\n)+?<\/ul>/',$this->page_content,$result);
+        preg_match_all('/<img src="(.+?)&amp;w=\d+&amp;zc=\d+;a=t"/',$result[0],$result);
+        foreach($result[1] as &$key){
+            $key .= $size;
+            unset($key);
+        }
+        return $result[1];
+    }
+    
+    function GetMangaInfo($id){
+        $page_content = file_get_contents(self::CE_URL."/{$id}");
+        preg_match('/href="(http:\/\/exhentai.org.+?)"/',$page_content,$result);
+        $ex_url = $result[1];
+        $this->ExInit();
+        $page_content = $this->ExLoadPage($ex_url);
+        preg_match('/Showing \d+ - \d+ of (?<num>\d+) images/',$page_content,$pic_num);
+        $pic_num = (int)$pic_num['num'];
+        preg_match('/<div id="gdt">.+?class="c"/',$page_content,$result);
+        $result = $result[0];
+        preg_match_all('/<img alt=".+?" title=".+?" src="(?<pic>.+?)"/',$result,$tmp);
+        $pics = $tmp['pic'];
+        preg_match_all('/<a href="(?<link>.+?)">/',$result,$tmp);
+        $links = $tmp['link'];
+        for ($i = 1; $i <= floor($pic_num / 20); $i++) {
+            $page_content = $this->ExLoadPage($ex_url."?p={$i}");
+            preg_match('/<div id="gdt">.+?class="c"/',$page_content,$result);
+            $result = $result[0];
+            preg_match_all('/<img alt=".+?" title=".+?" src="(?<pic>.+?)"/',$result,$tmp);
+            $pics = array_merge($pics,$tmp['pic']);
+            preg_match_all('/<a href="(?<link>.+?)">/',$result,$tmp);
+            $links = array_merge($links,$tmp['link']);
+        }
+        preg_match('/<div class="c6" id="comment_0".+?<\/div>/',$page_content,$introduction);
+        $introduction = $introduction[0];
+        $introduction = str_replace('更多资源请访问：https://cefamilie.com','',preg_replace('/(<.+?>)|\s/','',$introduction));
+        $mangas = $this->GetMangaPics($links);
+        return array('introduction' => $introduction,'thumbnail' => $pics,'manga' => $mangas);
+    }
+    
+    function GetMangaPics($links){
+        $mangas = array();
+        foreach($links as $id => $key){
+            $page_content = $this->ExLoadPage($key);
+            preg_match('/<img id="img" src="(?<pic>.+?)"/',$page_content,$tmp);
+            $mangas += array($id => $tmp['pic']);
+            sleep(1);
+        }
+        return $mangas;
+    }
+    
+    function ExInit(){
+        $this->cookie = file_get_contents($this->api_url);
+    }
+    
+    function ExLoadPage($url){
+        $ch = curl_init();//初始化curl
+        curl_setopt($ch,CURLOPT_COOKIE,$this->cookie); //设置cookie
+        curl_setopt($ch,CURLOPT_URL,$url);//抓取指定网页
+        curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
+        $page_content = curl_exec($ch);//运行curl
+        curl_close($ch);
+        return $page_content;
+    }
+}
